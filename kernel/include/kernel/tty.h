0fe22371aee94e3ff0bfe9905d6e6ab28321a1a8
Jacqueline <jackiewalkergeneral@gmail.com>
2023-11-03 02:04:45 +0000

    Expose VGA text color and reformatted tty.c/h
    Exposes a function that allows the terminal to change the foreground color of
    the terminal, called terminal_setcolor(). tty.c and tty.h have been reformatted
    to my coding style.

--- kernel/include/kernel/tty.h
+++ kernel/include/kernel/tty.h
@@ -2,10 +2,38 @@
 #define _KERNEL_TTY_H
 
 #include <stddef.h>
+#include <stdint.h>
 
-void terminal_initialize(void);
-void terminal_putchar(char c);
-void terminal_write(const char* data, size_t size);
-void terminal_writestring(const char* data);
+/* terminal_initialize()
+ * =====================
+ * Initializes the terminal that uses the VGA text mode buffer as the output
+ * device.
+ */
+void terminal_initialize( void );
+
+/* terminal_putchar()
+ * ==================
+ * Puts a character on the terminal buffer.
+ */
+void terminal_putchar( char c );
+
+/* terminal_write()
+ * ================
+ * Writes a character buffer to the terminal buffer.
+ */
+void terminal_write( const char* data, size_t size );
+
+/* terminal_writestring()
+ * ======================
+ * Writes a string literal to the terminal buffer.
+ */
+void terminal_writestring( const char* data );
+
+/* terminal_setcolor()
+ * ===================
+ * Sets the foreground color of the terminal.
+ * See vga.h for a full list of color constants.
+ */
+void terminal_setcolor( uint8_t color );
 
 #endif
